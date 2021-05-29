5c9f9b625aa9639ce8c359e68e4b53deb7ee6ea1
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-05-29 21:33:15 +0900

    update tsconfig.json - 型定義ファイルの出力先を変更

--- types/modules/Dialog.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export interface DialogProps {
-    content: React.ReactElement;
-    children: React.ReactChild;
-    onDialogOpened?: Function;
-}
-declare const Dialog: React.FunctionComponent<DialogProps>;
-export default Dialog;
